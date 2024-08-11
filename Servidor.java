5ffd17a132e751303a68ddf5edc7d6f57d52526a
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-11 13:17:37 -0300

    feat: Função de buscar aprimorada, implementada função de editar, operações geram logs.
    Caso um elemento seja encontrado na cache para a edição, a edição acontece na cache, já que o elemento é uma referencia.

--- Servidor.java
+++ Servidor.java
@@ -1,3 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Servidor {
     private Arvore arvore;
     private CacheFIFO cache;
@@ -6,63 +14,125 @@ public class Servidor {
         arvore = new Arvore();
         cache = new CacheFIFO();
     }
-    
-    public void cadastrarOrdemServico(OrdemServico os) {
-        arvore.setRaiz(arvore.inserir(arvore.getRaiz(), os));
-        System.out.println("Ordem de Serviço cadastrada: ");
-        System.out.println(os);
 
-        OrdemServico osBuscada = arvore.buscar(os.getCodigo()).os;
-        cache.adicionar(osBuscada); //colocando a referencia na cache
+    public void mostrarCache() {
+        System.out.println("Itens da Cache: ");
+        cache.listarCache();
+    }
+
+    public void cadastrarOrdemServico(OrdemServico os) {
+        escreverLog("");
+        escreverLog("Insercao de Ordem de Servico: " + os.getCodigo());
+        arvore.setRaiz(arvore.inserir(arvore.getRaiz(), os));
+
+        NoAVL noBuscado = arvore.buscar(os.getCodigo());
+        cache.adicionar(noBuscado); // colocando o no na cache
+
+        escreverLog("Altura da arvore: " + arvore.getRaiz().altura + ", Time: " + now());
+        escreverLog("Itens da cache: " + cache.gerarStringCache());
 
     }
 
-    public void removerOrdemServico(int codigo) {
+    public void removerOrdemServico(OrdemServico removeOS) {
 
+        escreverLog("");
+        escreverLog("Remoção de Ordem de Serviço: " + removeOS.getCodigo());
 
+        arvore.remover(removeOS);
+        cache.remover(removeOS.getCodigo());
+
+        
+        escreverLog("Altura da Árvore: " + arvore.getRaiz().altura + ", Time: " + now());
+        escreverLog("Itens da cache: " + cache.gerarStringCache());
 
     }
 
     public void atualizarOrdemServico(OrdemServico novaOS) {
+
+        NoAVL noBuscado = buscarNaCache(novaOS.getCodigo());
+
+        if (noBuscado != null) { // Encontrado na cache
+            noBuscado.os.setNome(novaOS.getNome());
+            noBuscado.os.setDescricao(novaOS.getDescricao());
+            noBuscado.os.setHoraSolicitacao(novaOS.getHoraSolicitacao());
+            System.out.println("Alterada na cache -> Árvore");
+
+            escreverLog("");
+            escreverLog("Alteracao na Ordem de Servido feita na cache: " + novaOS.getCodigo() + ", Time: " + now());
+            escreverLog("Itens da cache: " + cache.gerarStringCache());
+
+            return;
+        }
+
+        // Não tem na cache edita na árvore
         arvore.alterar(novaOS);
 
-        //Reload no item da cache
+        // Reload no item da cache
+        noBuscado = arvore.buscar(novaOS.getCodigo());
         cache.remover(novaOS.getCodigo());
-        cache.adicionar(novaOS);
+        cache.adicionar(noBuscado); // Armazena referencia
+
+        escreverLog("");
+        escreverLog("Alteracao na Ordem de Servico: " + novaOS.getCodigo() + ", Time: " + now());
+        escreverLog("Itens da cache: " + cache.gerarStringCache());
+
+    }
+
+    public NoAVL buscarNaCache(int codigo) {
+        NoAVL no = cache.buscar(codigo);
+        return no;
+
     }
 
     public OrdemServico buscarOrdemServico(int codigo) {
-        OrdemServico os = cache.buscar(codigo);
 
-        //Primeiro busca na cache
-        if (os != null) {
-            System.out.println("Ordem de Serviço encontrada na cache: ");
-            return os;
+        // Primeiro busca na cache
+        NoAVL buscado = buscarNaCache(codigo);
+        if (buscado != null) {
+            System.out.println("Ordem de Serviço encontrada na cache! ");
+            return buscado.os;
         }
 
-        //Não achou
-        
-        os = arvore.buscar(codigo).os;
+        // Não achou - busca na árvore
+        buscado = arvore.buscar(codigo);
 
-        if (os != null) { //Encontrou na árvore
-            cache.adicionar(os); // Adiciona na cache
-            System.out.println("Ordem de Serviço encontrada na base de dados (Árvore): " + os);
-            return os;
+        if (buscado != null) { // Encontrou na árvore
+            cache.adicionar(buscado); // Adiciona na cache
+            System.out.println("Ordem de Serviço encontrada na base de dados (Árvore)! ");
+
+            escreverLog("");
+            escreverLog("Item buscado foi adicionado na Cache: " + codigo + ", Time: " + now());
+            escreverLog("Itens da cache: " + cache.gerarStringCache());
+
+            return buscado.os;
         }
 
-        System.out.println("Ordem de Serviço não encontrada na base de dados.");
         return null;
     }
 
-    public void mostrarArvore(){
+    public void mostrarArvore() {
         System.out.println();
         this.arvore.verArvore();
         System.out.println();
     }
-    
-    public void mostrarOrdensServico(){
+
+    public void mostrarOrdensServico() {
         System.out.println();
         this.arvore.listarOS();
         System.out.println();
+    }
+
+    private String now() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.now().format(formatter);
+    }
+
+    private void escreverLog(String msg) {
+        try (BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream("log.txt", true), "UTF-8"))) {
+            writer.write(msg + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
