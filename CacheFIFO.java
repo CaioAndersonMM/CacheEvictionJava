9d3edb0356a1d99f17b35902791e62f6e34a638a
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-11 13:14:17 -0300

    perf: Cache armazena Nós AVL, ou seja, agora armazena a referencia dos nós da árvore.
    Agora a edição pode ser feita nos nós dentro da cache. Além disso, foi criado um método para gerar uma string dos nós da cache no arquivo de log.

--- CacheFIFO.java
+++ CacheFIFO.java
@@ -2,36 +2,44 @@ import java.util.LinkedList;
 
 public class CacheFIFO {
     private final int maxSize;
-    private LinkedList<OrdemServico> cache;
+    private LinkedList<NoAVL> cache;
 
     public CacheFIFO() {
-        this.maxSize = 10;
+        this.maxSize = 21;
         this.cache = new LinkedList<>();
     }
 
-    public void adicionar(OrdemServico os) {
+    public void adicionar(NoAVL os) {
         if (cache.size() >= maxSize) {
             cache.removeFirst();
         }
         cache.addLast(os);
     }
 
-    public OrdemServico buscar(int codigo) {
-        for (OrdemServico os : cache) {
-            if (os.getCodigo() == codigo) {
-                return os;
+    public NoAVL buscar(int codigo) {
+        for (NoAVL no : cache) {
+            if (no.os.getCodigo() == codigo) { 
+                return no;
             }
         }
         return null;
     }
 
     public void listarCache() {
-        for (OrdemServico os : cache) {
-            System.out.println(os);
+        for (NoAVL no : cache) {
+            System.out.println(no.os);
         }
     }
 
     public void remover(int codigo) {
-        cache.removeIf(os -> os.getCodigo() == codigo);
+        cache.removeIf(no -> no.os.getCodigo() == codigo);
+    }
+
+    public String gerarStringCache() {
+        StringBuilder sb = new StringBuilder();
+        for (NoAVL no : cache) {
+            sb.append(no.os.imprimir()).append(", ");
+        }
+        return sb.length() > 0 ? sb.substring(0, sb.length() - 2) : "Cache vazia";
     }
 }
