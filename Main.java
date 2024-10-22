d8437ab3b48d56f1222bbe1760f3c25c2b18dc9e
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-10-22 14:18:37 -0300

    feat/update: Tudo é enviado ao servidor como mensagem compactada, adicionado método de buscar textos no log.

--- Main.java
+++ Main.java
@@ -21,6 +21,7 @@ public class Main {
                 System.out.println("| 6 | - Buscar Ordem de Serviço");
                 System.out.println("| 7 | - Ver Cache");
                 System.out.println("| 8 | - Ver Mod e Fator de Carga");
+                System.out.println("| 9 | - Buscar ocorrências no log");
                 System.out.println("| 0 | - Sair");
 
                 int escolha = sc.nextInt();
@@ -46,15 +47,15 @@ public class Main {
 
                         System.out.println("Digite a hora da solicitação (formato HHMM):");
                         String hora = sc.nextLine();
-                        sc.nextLine();
 
-                        //Compressão acontece no construtor
+                        // Compressão acontece no construtor
                         Mensagem mensagem = new Mensagem(codInserir, "Cadastrar", nome, descricao, hora);
-                        server.processarMensagem(mensagem);
                         System.out.println("Mensagem enviada ao servidor.");
+                        server.processarMensagem(mensagem);
                         break;
                     case 2:
-                        server.mostrarTabelaHash();
+                        Mensagem mensagemListar = new Mensagem(0, "Listar", "", "", "");
+                        server.processarMensagem(mensagemListar);
                         break;
                     case 3:
                         System.out.println("Digite o código da Ordem de Serviço para alterar:");
@@ -72,15 +73,11 @@ public class Main {
                             String descricaoEdit = sc.nextLine();
 
                             System.out.println("Edite a nova hora da solicitação (formato HHMM):");
-                            String horaEdit = sc.nextLine();                            sc.nextLine();
-
-                            String horaSolicitacao = String.valueOf(osEdit.getHoraSolicitacao());
-
+                            String horaEdit = sc.nextLine();
 
                             Mensagem mensagemAlterar = new Mensagem(codEdit, "Alterar", nomeEdit, descricaoEdit, horaEdit);
-
-                            server.processarMensagem(mensagemAlterar);
                             System.out.println("Mensagem enviada ao servidor.");
+                            server.processarMensagem(mensagemAlterar);
                         } else {
                             System.out.println("Ordem de Serviço não encontrada!");
                         }
@@ -91,42 +88,47 @@ public class Main {
                         int codRemove = sc.nextInt();
                         sc.nextLine();
 
-                        OrdemServico osRemove = server.buscarOrdemServico(codRemove, true);
-                        if (osRemove != null) {
-                            Mensagem mensagemExcluir = new Mensagem(codRemove, "Remover", osRemove.getNome(), osRemove.getDescricao(), String.valueOf(osRemove.getHoraSolicitacao()));
+                        Mensagem mensagemRemover = new Mensagem(codRemove, "Remover", "", "", "");
 
-                            server.processarMensagem(mensagemExcluir);
-                            System.out.println("Mensagem enviada ao servidor.");
-                        } else {
-                            System.out.println("Ordem de Serviço não encontrada.");
-                        }
+                        System.out.println("Mensagem enviada ao servidor.");
+                        server.processarMensagem(mensagemRemover);
+
                         break;
 
                     case 5:
-                        System.out.println("Quantidade de Registros: " + server.quantidadeDeRegistros());
+                        Mensagem mensagemQuantidade = new Mensagem(0, "Quantidade", "", "", "");
+                        System.out.println("Mensagem enviada ao servidor.");
+                        server.processarMensagem(mensagemQuantidade);
                         break;
 
                     case 6:
                         System.out.println("Qual Ordem de Serviço você quer buscar? ");
                         int codBuscar = sc.nextInt();
-                        OrdemServico osBuscada = server.buscarOrdemServico(codBuscar, false);
-                        if (osBuscada != null) {
-                            System.out.println(osBuscada);
-                        } else {
-                            System.out.println("Ordem de Serviço não encontrada.");
-                        }
-
+                        Mensagem mensagemBuscar = new Mensagem(codBuscar, "Buscar", "", "", "");
+                        System.out.println("Mensagem enviada ao servidor.");
+                        server.processarMensagem(mensagemBuscar);
                         break;
 
                     case 7:
-                        server.mostrarCache();
+                        Mensagem mensagemCache = new Mensagem(0, "Cache", "", "", "");
+                        System.out.println("Mensagem enviada ao servidor.");
+                        server.processarMensagem(mensagemCache);
                         break;
 
                     case 8:
-                        System.out.println("Mod: " + server.mostrarMod() + " Carga: "
-                                + String.format("%.2f", server.mostrarFatorDeCarga()) + "%");
+                        Mensagem mensagemMod = new Mensagem(0, "Mod", "", "", "");
+                        System.out.println("Mensagem enviada ao servidor.");
+                        server.processarMensagem(mensagemMod);
                         break;
-
+                    case 9:
+                        System.out.println("Você quer buscar por 1-Inserções, 2-Remoções, 3-Alterações ou 4-Buscas? ");
+                        int escolhaLog = sc.nextInt();
+                        
+                        Mensagem mensagemLog = new Mensagem(escolhaLog, "VerLog", "", "", "");
+                        System.out.println("Mensagem enviada ao servidor.");
+                        server.processarMensagem(mensagemLog);
+                        
+                        break;
                     case 0:
                         option = false;
                     default:
@@ -148,11 +150,12 @@ public class Main {
 
     public static Servidor gerarOrdensFicticias(Servidor server) {
 
-        for (int i = 0; i <= 69; i++) {
-            // OrdemServico os = new OrdemServico(i, "Serviço " + i, "Descrição do serviço " + i, (i * 2) % 24);
+        for (int i = 0; i <= 99; i++) {
+            // OrdemServico os = new OrdemServico(i, "Serviço " + i, "Descrição do serviço "
+            // + i, (i * 2) % 24);
             // server.cadastrarOrdemServico(os);
 
-            Mensagem mensagem = new Mensagem(i, "Cadastrar", "Serviço " + i, "Descrição da " + i,  "" + i + ":" + i);
+            Mensagem mensagem = new Mensagem(i, "Cadastrar", "Serviço " + i, "Descrição da " + i, "" + i + ":" + i);
             server.processarMensagem(mensagem);
             System.out.println("Mensagem enviada ao servidor.");
         }
